aa9654bd4f29fd8cd0a31e39a581640dcda365a1
galkovsky <galkovsky92@gmail.com>
2012-04-18 00:35:58 +0300

    Added vertical layout

--- DND_Layout/src/main/java/layout/MyLayout.java
+++ DND_Layout/src/main/java/layout/MyLayout.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package layout;
 
@@ -14,10 +14,16 @@ import java.awt.LayoutManager;
  */
 public class MyLayout implements LayoutManager {
 	private int vgap;
+	private int minWidth = 0, minHeight = 0;
 	private int preferredWidth = 0, preferredHeight = 0;
 	private boolean sizeUnknown = true;
 
 	public MyLayout() {
+		this(5);
+	}
+
+	public MyLayout(int v) {
+		vgap = v;
 	}
 
 	/* Required by LayoutManager. */
@@ -30,40 +36,37 @@ public class MyLayout implements LayoutManager {
 
 	private void setSizes(Container parent) {
 		int nComps = parent.getComponentCount();
-
 		Dimension d = null;
 
 		// Reset preferred/minimum width and height.
-		int lastCollHeight = 0;
 		preferredWidth = 0;
 		preferredHeight = 0;
-
+		minWidth = 0;
+		minHeight = 0;
 
 		for (int i = 0; i < nComps; i++) {
 			Component c = parent.getComponent(i);
-			if (!c.isVisible()) {
-				continue;
-			}
+			if (c.isVisible()) {
+				d = c.getPreferredSize();
 
-			d = c.getPreferredSize();
-			if (lastCollHeight < parent.getHeight()) {
-				lastCollHeight += d.height;
-			} else {
-				preferredHeight = lastCollHeight;
-				preferredWidth += d.width;
-				lastCollHeight = 0;
-			}
+				if (i > 0) {
+					preferredWidth += d.width / 2;
+					preferredHeight += vgap;
+				} else {
+					preferredWidth = d.width;
+				}
+				preferredHeight += d.height;
 
-		}
-		if (preferredHeight < lastCollHeight) {
-			preferredHeight = lastCollHeight;
+				minWidth = Math.max(c.getMinimumSize().width, minWidth);
+				minHeight = preferredHeight;
+			}
 		}
 	}
 
 	/* Required by LayoutManager. */
 	public Dimension preferredLayoutSize(Container parent) {
-		System.out.println("FDF1111");
 		Dimension dim = new Dimension(0, 0);
+		int nComps = parent.getComponentCount();
 
 		setSizes(parent);
 
@@ -77,50 +80,94 @@ public class MyLayout implements LayoutManager {
 		return dim;
 	}
 
-	public void layoutContainer(Container parent) {
+	/* Required by LayoutManager. */
+	public Dimension minimumLayoutSize(Container parent) {
+		Dimension dim = new Dimension(0, 0);
+		int nComps = parent.getComponentCount();
 
+		// Always add the container's insets!
+		Insets insets = parent.getInsets();
+		dim.width = minWidth + insets.left + insets.right;
+		dim.height = minHeight + insets.top + insets.bottom;
+
+		sizeUnknown = false;
+
+		return dim;
+	}
+
+	/* Required by LayoutManager. */
+	/*
+	 * This is called when the panel is first displayed, and every time its size
+	 * changes. Note: You CAN'T assume preferredLayoutSize or minimumLayoutSize
+	 * will be called -- in the case of applets, at least, they probably won't
+	 * be.
+	 */
+	public void layoutContainer(Container parent) {
+		Insets insets = parent.getInsets();
+		int maxWidth = parent.getWidth() - (insets.left + insets.right);
+		int maxHeight = parent.getHeight() - (insets.top + insets.bottom);
+		int nComps = parent.getComponentCount();
+		int previousWidth = 0, previousHeight = 0;
+		int x = 0, y = insets.top;
+		int rowh = 0, start = 0;
+		int xFudge = 0, yFudge = 0;
+		boolean oneColumn = false;
+
+		// Go through the components' sizes, if neither
+		// preferredLayoutSize nor minimumLayoutSize has
+		// been called.
 		if (sizeUnknown) {
 			setSizes(parent);
 		}
-		int lastY = 0;
-		int lastX = 0;
-		int nComps = parent.getComponentCount();
+
+		if (maxWidth <= minWidth) {
+			oneColumn = true;
+		}
+
+		if (maxWidth != preferredWidth) {
+			xFudge = (maxWidth - preferredWidth) / (nComps - 1);
+		}
+
+		if (maxHeight > preferredHeight) {
+			yFudge = (maxHeight - preferredHeight) / (nComps - 1);
+		}
 
 		for (int i = 0; i < nComps; i++) {
 			Component c = parent.getComponent(i);
-			if (!c.isVisible()) {
-				continue;
-			}
-			Dimension d = c.getPreferredSize();
-			if (parent.getHeight() < lastY + d.height) {
-				System.out.println("FUCK");
-				lastY = 0;
-				lastX += d.width;
+			if (c.isVisible()) {
+				Dimension d = c.getPreferredSize();
 
-			}
-			c.setBounds(lastX, lastY, d.width, d.height);
+				// increase x and y, if appropriate
+				if (i > 0) {
+					if (!oneColumn) {
+						x += previousWidth / 2 + xFudge;
+					}
+					y += previousHeight + vgap + yFudge;
+				}
 
-			lastY += d.height;
-//			System.out.println(lastY);
+				// If x is too large,
+				if ((!oneColumn) && (x + d.width) > (parent.getWidth() - insets.right)) {
+					// reduce x to a reasonable number.
+					x = parent.getWidth() - insets.bottom - d.width;
+				}
+
+				// If y is too large,
+				if ((y + d.height) > (parent.getHeight() - insets.bottom)) {
+					// do nothing.
+					// Another choice would be to do what we do to x.
+				}
+
+				// Set the component's size and position.
+				c.setBounds(x, y, d.width, d.height);
+
+				previousWidth = d.width;
+				previousHeight = d.height;
+			}
 		}
-		System.out.print(preferredLayoutSize(parent).height + " : ");
-		System.out.println(preferredLayoutSize(parent).width);
-		System.out.print(parent.getHeight() + " : ");
-		System.out.println(parent.getWidth());
-		System.out.println();
 	}
 
 	public String toString() {
 		String str = "";
 		return getClass().getName() + "[vgap=" + vgap + str + "]";
-	}
-
-	/* (non-Javadoc)
-	 * @see java.awt.LayoutManager#minimumLayoutSize(java.awt.Container)
-	 */
-	@Override
-	public Dimension minimumLayoutSize(Container parent) {
-		System.out.println("FDF");
-		return preferredLayoutSize(parent);
 	}
 }
